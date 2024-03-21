6ff02e58cf954d53f44be26992fa4700322eb515
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-21 14:03:17 +0530

    fix

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,27 +5,27 @@ const Card = (props) =>{
     let review = props.review;
 
     return(
-        <div className ="flex flex-col md:relative">
+        <div className ="flex flex-col md:relative ">
             <div className = "absolute top-[-7rem] z-[10] mx-auto ">
                 <img className = "aspect-square rounded-full w-[140px] h-[140px] z-[25]" src = {review.image}/>
 <div className = "w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[10px]"></div>
             </div>
         <div className = "text-center mt-7">
-            <p className = 'tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
-            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+            <p className = 'tracking-wider font-bold text-2xl capitalize text-white underline decoration-solid'>{review.name}</p>
+            <p className='text-violet-800 uppercase text-sm'>{review.job}</p>
 
         </div>
 
-        <div className = 'text-center mt-7'>
+        <div className = 'text-center bg-white mt-7'>
             
         </div>
-        <div className='text-violet-400 mx-auto mt-5 '>
+        <div className='text-violet-800 mx-auto mt-5 '>
             <FaQuoteLeft/>
         </div>
-        <div className = 'text-center mt-4 text-slate-500'>
+        <div className = 'text-center mt-4 text-white underline "'>
             {review.text}
         </div>
-        <div className='text-violet-400 mx-auto mt-5'>
+        <div className='text-violet-800 mx-auto mt-5'>
             <FaQuoteRight/>
         </div>
         
--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -37,21 +37,21 @@ import {useState} from 'react';
 
     }
     return(
-        <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md '>
+        <div className='w-[85vw] md:w-[700px] bg-black flex flex-col justify-center mt-10 p-10 transition-all duration-700  rounded-3xl  '>
             <Card review = {reviews[index]}/>
             
         <div className='flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto'>
             <button onClick = {leftShiftHandler}
-            className = "cursor-pointer hover:text-violet-500">
+            className = "cursor-pointer hover:text-violet-800">
                 <FiChevronLeft/>
                 
             </button>
-            <button onClick = {rightShiftHandler} className = "cursor-pointer hover:text-violet-500">
+            <button onClick = {rightShiftHandler} className = "cursor-pointer hover:text-violet-800">
                 <FiChevronRight/>
             </button>
             </div>
             <div className = "mt-6">
-            <button   onClick = {surpriseHandler} className = 'bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>Surprise me!!</button>
+            <button  onClick = {surpriseHandler} className = 'bg-violet-600 hover:bg-violet-800 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>Surprise me!!</button>
         </div>
 
 
