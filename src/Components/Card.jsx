d973b3eadcc980b5a36b3d2fab4629f81c71001c
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-21 13:19:51 +0530

    commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+
 import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
-import {FiChevronLeft , FiChevronRight} from 'react-icons/fi';
 const Card = (props) =>{
     let review = props.review;
 
@@ -11,12 +11,13 @@ const Card = (props) =>{
 <div className = "w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[10px]"></div>
             </div>
         <div className = "text-center mt-7">
-            <p className = 'font-bold text-2xl capitalize'>{review.name}</p>
+            <p className = 'tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
+            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
 
         </div>
 
         <div className = 'text-center mt-7'>
-            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+            
         </div>
         <div className='text-violet-400 mx-auto mt-5 '>
             <FaQuoteLeft/>
@@ -24,19 +25,10 @@ const Card = (props) =>{
         <div className = 'text-center mt-4 text-slate-500'>
             {review.text}
         </div>
-        <div>
+        <div className='text-violet-400 mx-auto mt-5'>
             <FaQuoteRight/>
         </div>
-        <div className='flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto'>
-            <button className = "cursor-pointer hover:text-violet-500">
-                <FiChevronLeft/>
-                
-            </button>
-            <button className = "cursor-pointer hover:text-violet-500">
-                <FiChevronRight/>
-            </button>
-            <button>Surprise me!!</button>
-        </div>
+        
         </div>
         )
 
