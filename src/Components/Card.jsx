3d5e38e3632f9895c0efe613f864363b25a046be
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-21 13:33:01 +0530

    first commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) =>{
     return(
         <div className ="flex flex-col md:relative">
             <div className = "absolute top-[-7rem] z-[10] mx-auto ">
-                <img className = "aspect-square rounded-full w-[140px] h-[140px] z-[25]" src = {"download.jpeg"}/>
+                <img className = "aspect-square rounded-full w-[140px] h-[140px] z-[25]" src = {review.image}/>
 <div className = "w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-20] left-[10px]"></div>
             </div>
         <div className = "text-center mt-7">
